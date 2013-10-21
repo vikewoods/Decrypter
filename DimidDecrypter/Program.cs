7800fe4da9b73f3f942175e3115665fa1954f3e9
Vik Ewoods <vikewoods@viks-mbp.dlink>
2013-10-22 00:23:57 +0300

    Added validation of base64 string for decrypt

--- DimidDecrypter/Program.cs
+++ DimidDecrypter/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
+
 
 namespace DimidDecrypter
 {
   class MainClass
   {
+    private static readonly HashSet<char> _base64Characters = new HashSet<char>() { 
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+      'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
+      'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+      'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/', 
+      '='
+    };
+
     public static void Main (string[] args)
     {
       //Console.WriteLine ("Hello World!");
@@ -26,9 +37,13 @@ namespace DimidDecrypter
         if(inputFromConsole == "2"){
           Console.Write ("Input base64 string: ");
           string inputForDecrypt = Console.ReadLine ();
-          byte[] decrypted64 = Convert.FromBase64String (inputForDecrypt);
-          string result = System.Text.Encoding.UTF8.GetString (decrypted64);
-          Console.WriteLine ("Decrypted text: " + result);
+          while(IsBase64String(inputForDecrypt)){
+            byte[] decrypted64 = Convert.FromBase64String (inputForDecrypt);
+            string result = System.Text.Encoding.UTF8.GetString (decrypted64);
+            Console.WriteLine ("Decrypted text: " + result);
+          }
+          Console.WriteLine ("Input is not valid base64 string!");
+
         }// If input 2 end
         // If input equal 3 lets terminate application
         if(inputFromConsole == "3"){
@@ -36,5 +51,27 @@ namespace DimidDecrypter
         }
       }// Loop
     }// Main function
+   
+    public static bool IsBase64String(string value)
+    {
+      if (string.IsNullOrEmpty(value))
+      {
+        return false;
+      }
+      else if (value.Any(c => !_base64Characters.Contains(c)))
+      {
+        return false;
+      }
+
+      try
+      {
+        Convert.FromBase64String(value);
+        return true;
+      }
+      catch (FormatException)
+      {
+        return false;
+      }
+    }
   }
 }
